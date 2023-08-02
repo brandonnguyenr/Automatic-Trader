93a1c625363b24e6a976ad80d3e65b239903ea92
Brandon Nguyen <nguyen.brandon771@gmail.com>
2023-08-01 19:44:31 -0700

    update

--- model/bollingerBandBounce.py
+++ model/bollingerBandBounce.py
@@ -6,11 +6,14 @@ import numpy as np
 import DownloadData as dd
 
 def bollingerBB():
-    stock = dd.DownloadData('FNGU','07262020','07262023')
-    ticker = 'FNGU'
-    period1 = int(time.mktime(datetime.datetime(2020, 7, 26, 23, 59).timetuple()))
-    period2 = int(time.mktime(datetime.datetime(2023, 7, 26, 23, 59).timetuple()))
-    interval = '1d'  
+    etf = input("Enter ETF (FNGD/FNGU): ")
+    date1 = input("Enter start date (MMDDYYYY): ")
+    date2 = input("Enter end date (MMDDYYYY): ")
+    stock = dd.DownloadData(etf,date1,date2)
+    ticker = etf
+    period1 = int(time.mktime(datetime.datetime(int(date1[-4:]), int(date1[:2]), int(date1[2:4]), 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(int(date2[-4:]), int(date2[:2]), int(date2[2:4]), 23, 59).timetuple()))
+    interval = '1d'
 
     API_endpoint = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
 
@@ -25,33 +28,46 @@ def bollingerBB():
 
     initial_capital = 100000
     capital = initial_capital
-    position = 0  
+    position = 0
     buy_price = 0
+    buy_date = None
+    sell_date = None
+
+    df['Buy'] = False
+    df['Sell'] = False
 
     for index, row in df.iterrows():
-        if row['Close'] < row['Lower'] and position == 0:  
+        if row['Close'] < row['Lower'] and position == 0:
             position = 1
             buy_price = row['Close']
+            buy_date = row['Date']
             shares = capital / buy_price
             capital = 0
-        elif row['Close'] > row['Upper'] and position == 1:  
+            df.loc[index, 'Buy'] = True
+            print(f"Buy: {buy_date} | Price: ${buy_price:.2f}")
+        elif row['Close'] > row['Upper'] and position == 1:
             position = 0
             sell_price = row['Close']
+            sell_date = row['Date']
             capital = shares * sell_price
             buy_price = 0
+            df.loc[index, 'Sell'] = True
+            print(f"Sell: {sell_date} | Price: ${sell_price:.2f}")
 
     if position == 1:
         position = 0
         sell_price = df['Close'].iloc[-1]
         capital = shares * sell_price
         buy_price = 0
+        df.loc[df.index[-1], 'Sell'] = True
+        print(f"Sell: {df['Date'].iloc[-1]} | Price: ${sell_price:.2f}")
 
     final_profit_loss = capital - initial_capital
     percent_profit_margin = (final_profit_loss / initial_capital) * 100
 
-    print(f"Final Capital: ${capital:.2f}")
+    print(f"\nFinal Capital: ${capital:.2f}")
     print(f"Profit/Loss: ${final_profit_loss:.2f}")
-    print(f"Percent Profit Margin: {percent_profit_margin:.2f}%")
+    print(f"Percent Profit Margin: {percent_profit_margin:.2f}%\n")
 
     xpoints = stock.getDate()
     ypoints = stock.getClose()
@@ -62,6 +78,10 @@ def bollingerBB():
     plt.plot(xpoints, df['MA'], label=f'{window}-day MA', color='orange')
     plt.fill_between(xpoints, df['Upper'], df['MA'], alpha=0.2, color='green', label='Upper Bollinger Band')
     plt.fill_between(xpoints, df['MA'], df['Lower'], alpha=0.2, color='red', label='Lower Bollinger Band')
+
+    # Add buy/sell points to the graph
+    plt.scatter(df[df['Buy'] == True]['Date'], df[df['Buy'] == True]['Close'], marker='^', color='g', label='Buy', lw=0)
+    plt.scatter(df[df['Sell'] == True]['Date'], df[df['Sell'] == True]['Close'], marker='v', color='r', label='Sell', lw=0)
 
     plt.xlabel('Date')
     plt.ylabel('Closing Price')
--- model/movingAverageCrossover.py
+++ model/movingAverageCrossover.py
@@ -6,10 +6,13 @@ import numpy as np
 import DownloadData as dd
 
 def mva():
-    stock = dd.DownloadData('FNGU','07262020','07262023')
-    ticker = 'FNGU'
-    period1 = int(time.mktime(datetime.datetime(2020, 7, 26, 23, 59).timetuple()))
-    period2 = int(time.mktime(datetime.datetime(2023, 7, 26, 23, 59).timetuple()))
+    etf = input("Enter ETF (FNGD/FNGU): ")
+    date1 = input("Enter start date (MMDDYYYY): ")
+    date2 = input("Enter end date (MMDDYYYY): ")
+    stock = dd.DownloadData(etf,date1,date2)
+    ticker = etf
+    period1 = int(time.mktime(datetime.datetime(int(date1[-4:]), int(date1[:2]), int(date1[2:4]), 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(int(date2[-4:]), int(date2[:2]), int(date2[2:4]), 23, 59).timetuple()))
     interval = '1d'  
 
     API_endpoint = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
@@ -28,31 +31,44 @@ def mva():
     capital = initial_capital
     position = 0  
     buy_price = 0
+    buy_date = None
+    sell_date = None
+
+    df['Buy'] = False
+    df['Sell'] = False
 
     for index, row in df.iterrows():
         if row['Signal'] == 1 and position == 0:  
             position = 1
             buy_price = row['Close']
+            buy_date = row['Date']
             shares = capital / buy_price
             capital = 0
+            df.loc[index, 'Buy'] = True
+            print(f"Buy: {buy_date} | Price: ${buy_price:.2f}")
         elif row['Signal'] == -1 and position == 1:  
             position = 0
             sell_price = row['Close']
+            sell_date = row['Date']
             capital = shares * sell_price
             buy_price = 0
+            df.loc[index, 'Sell'] = True
+            print(f"Sell: {sell_date} | Price: ${sell_price:.2f}")
 
     if position == 1:
         position = 0
         sell_price = df['Close'].iloc[-1]
         capital = shares * sell_price
         buy_price = 0
+        df.loc[df.index[-1], 'Sell'] = True
+        print(f"Sell: {df['Date'].iloc[-1]} | Price: ${sell_price:.2f}")
 
     final_profit_loss = capital - initial_capital
     percent_profit_margin = (final_profit_loss / initial_capital) * 100
 
-    print(f"Final Capital: ${capital:.2f}")
+    print(f"\nFinal Capital: ${capital:.2f}")
     print(f"Profit/Loss: ${final_profit_loss:.2f}")
-    print(f"Percent Profit Margin: {percent_profit_margin:.2f}%")
+    print(f"Percent Profit Margin: {percent_profit_margin:.2f}%\n")
 
     xpoints = stock.getDate()
     ypoints = np.array(df['Close'])
@@ -62,6 +78,10 @@ def mva():
 
     plt.plot(xpoints, df['Fast_MA'], label=f'{fast_window}-day Fast MA', color='orange')
     plt.plot(xpoints, df['Slow_MA'], label=f'{slow_window}-day Slow MA', color='red')
+
+    # Add buy/sell points to the graph
+    plt.scatter(df[df['Buy'] == True]['Date'], df[df['Buy'] == True]['Close'], marker='^', color='g', label='Buy', lw=0)
+    plt.scatter(df[df['Sell'] == True]['Date'], df[df['Sell'] == True]['Close'], marker='v', color='r', label='Sell', lw=0)
 
     plt.xlabel('Date')
     plt.ylabel('Closing Price')
