55b1a8f207fe9208f638d6565bfbf0e8e698f007
Kevin <kevinnguyenasdf@yahoo.com>
2023-08-02 17:39:15 -0700

    refactored

--- controller/main.py
+++ controller/main.py
@@ -6,8 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'v
 
 from DownloadData import DownloadData as dd
 from Display import Display as display
-from movingAverageCrossover import mva
-from bollingerBandBounce import bollingerBB
+from Backtest import Backtest as bt
 
 def main():
     print("*****Welcome to FN Trader*****")
@@ -23,31 +22,50 @@ def main():
                     date2 = input("Enter end date (MMDDYYYY): ")
                     stock = dd('FNGD', date1, date2)
                     d = display(stock)
-                    d.generateGraph()
+                    d.generateHistoricalGraph()
                 elif user == "2":
                     date1 = input("Enter start date (MMDDYYYY): ")
                     date2 = input("Enter end date (MMDDYYYY): ")
                     stock = dd('FNGU', date1, date2)
                     d = display(stock)
-                    d.generateGraph()
+                    d.generateHistoricalGraph()
                 elif user.lower() == "r":
                     break
                 else:
                     print("Invalid try again")
 
         elif user == "2":
+
             while True:
                 user = input("1. Moving Average Crossover\n2. Bollinger Band Bounce\nR. Return\nChoice: ")
                 if user == "1":
-                    mva()
+                    etf = input("Enter ETF (FNGD/FNGU): ")
+                    date1 = input("Enter start date (MMDDYYYY): ")
+                    date2 = input("Enter end date (MMDDYYYY): ")
+                    fast_window = 7 
+                    slow_window = 30  
+                    stock = dd(etf,date1,date2)
+                    test = bt(stock,date1,date2)
+                    test.testMovingAverageCrossover(slow_window=slow_window,fast_window=fast_window)
+
                 elif user == "2":
-                    bollingerBB()
+                    etf = input("Enter ETF (FNGD/FNGU): ")
+                    date1 = input("Enter start date (MMDDYYYY): ")
+                    date2 = input("Enter end date (MMDDYYYY): ")
+                    window = 15
+                    stock = dd(etf,date1,date2)
+                    test = bt(stock,date1,date2)
+                    test.testBollingerBandBounce(window=window)
+
                 elif user.lower() == "r":
                     break
+
                 else:
                     print("Invalid try again")
+
         elif user.lower() == "q":
             quit()
+
         else:
             print("Invalid input, try again!")
 
--- view/Display.py
+++ view/Display.py
@@ -1,12 +1,11 @@
 
 import matplotlib.pyplot as plt
 
-
 class Display:
     def __init__(self,stock):
         self.stock = stock
 
-    def generateGraph(self):
+    def generateHistoricalGraph(self):
 
         xpoints = self.stock.getDate()
         ypoints = self.stock.getClose()
@@ -25,3 +24,50 @@ class Display:
         plt.ylabel('Close USD')
         plt.title(self.stock.getName())
         plt.show()
+
+    def generateBollingerBBGraph(self,df):
+        window = 15
+        xpoints = self.stock.getDate()
+        ypoints = self.stock.getClose()
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(xpoints, ypoints, label='Closing Prices', color='blue')
+
+        plt.plot(xpoints, df['MA'], label=f'{window}-day MA', color='orange')
+        plt.fill_between(xpoints, df['Upper'], df['MA'], alpha=0.2, color='green', label='Upper Bollinger Band')
+        plt.fill_between(xpoints, df['MA'], df['Lower'], alpha=0.2, color='red', label='Lower Bollinger Band')
+
+        # Add buy/sell points to the graph
+        plt.scatter(df[df['Buy'] == True]['Date'], df[df['Buy'] == True]['Close'], marker='^', color='g', label='Buy', lw=0)
+        plt.scatter(df[df['Sell'] == True]['Date'], df[df['Sell'] == True]['Close'], marker='v', color='r', label='Sell', lw=0)
+
+        plt.xlabel('Date')
+        plt.ylabel('Closing Price')
+        plt.title('Bollinger Band Bounce Trading Strategy')
+        plt.xticks(rotation=45)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def generateMovingAverageCrossoverGraph(self,df,slow_window,fast_window):
+        xpoints = self.stock.getDate()
+        ypoints = self.stock.getClose()
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(xpoints, ypoints, label='Closing Prices', color='blue')
+
+        plt.plot(xpoints, df['Fast_MA'], label=f'{fast_window}-day Fast MA', color='orange')
+        plt.plot(xpoints, df['Slow_MA'], label=f'{slow_window}-day Slow MA', color='red')
+
+        # Add buy/sell points to the graph
+        plt.scatter(df[df['Buy'] == True]['Date'], df[df['Buy'] == True]['Close'], marker='^', color='g', label='Buy', lw=0)
+        plt.scatter(df[df['Sell'] == True]['Date'], df[df['Sell'] == True]['Close'], marker='v', color='r', label='Sell', lw=0)
+
+        plt.xlabel('Date')
+        plt.ylabel('Closing Price')
+        plt.title('Moving Crossover Trading Strategy')
+        plt.xticks(rotation=45)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
