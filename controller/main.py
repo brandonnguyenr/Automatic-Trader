c550d3141a7faea219bf86ce70de17d51e8e6342
Kevin <kevinnguyenasdf@yahoo.com>
2023-08-03 19:19:52 -0700

    changes to main

--- controller/main.py
+++ controller/main.py
@@ -9,6 +9,7 @@ from DownloadData import DownloadData as dd
 from Display import Display as display
 from movingAverageCrossover import MovingAverageCrossover as mva
 from bollingerBandBounce import BollingerBandBounce as bb
+
 def calculateDaysBefore(date,daysBefore):
     try:
         # Parse the input date string into a datetime object
@@ -48,12 +49,12 @@ def inputDate(message):
     while True:
         result = input(message)
         if result.isnumeric() == False:
-            print("Invalid! Try again")
+            print("Invalid! Try again.")
             continue
         if result.isnumeric() and validDate(result):
             return result
         else:
-            print("Invalid! Try again")
+            print("Invalid! Try again.")
 
 def validRange(date1,date2):
     dateFormat = "%m%d%Y"
@@ -64,7 +65,15 @@ def validRange(date1,date2):
         return False
     else:
         return True
-
+    
+def inputWindow(message):
+    print("\nFor default value enter non numeric values")
+    result = input(message)
+    if result.isnumeric() == True:
+        return int(result)
+    else:
+        return -1
+        
 def backtestInterface():
     while True:
         user = input("1. Moving Average Crossover\n2. Bollinger Band Bounce\nR. Return\nChoice: ")
@@ -75,14 +84,20 @@ def backtestInterface():
             if validRange(date1,date2) == False:
                 print("Invalid range! Try again\n")
                 continue
-            fast_window = 7 
-            slow_window = 30  
+
+            fast_window = inputWindow("Enter fast window range: ") 
+            if fast_window == -1:
+                fast_window = 7
+            slow_window = inputWindow("Enter slow window range: ")
+            if slow_window == -1:
+                slow_window = 30
             date1 = calculateDaysBefore(date=date1,daysBefore=slow_window)
             stock = dd(etf,date1,date2)
             # test = bt(stock,date1,date2)
             # test.testMovingAverageCrossover(slow_window=slow_window,fast_window=fast_window)
             test = mva(stock,date1,date2,slow=slow_window,fast=fast_window)
             test.testStrategy()
+
         elif user == "2":
             etf = inputETF()
             date1 = inputDate("Enter start date (MMDDYYYY): ")
@@ -90,7 +105,9 @@ def backtestInterface():
             # if validRange(date1,date2) == False:
             #     print("Invalid range! Try again\n")
             #     continue
-            window = 15
+            window = inputWindow("Enter window range: ")
+            if window == -1:
+                window = 15
             date1 = calculateDaysBefore(date=date1,daysBefore=window)
             stock = dd(etf,date1,date2)
             # test = bt(stock,date1,date2)
